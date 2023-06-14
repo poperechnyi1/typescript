d72adfa3ac4eee8fd9a172f1b4800fdca6f10c35
Stepan Poperechnyi <poperechnyi1@gmail.com>
2023-06-14 19:36:56 +0300

    DI project

---
+++ di/src/disk/disk.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@nestjs/common';
+import { PowerService } from '../power/power.service';
+
+@Injectable()
+export class DiskService {
+  constructor(private powerService: PowerService) {
+
+  }
+
+  getData() {
+    console.log('Drawing 20 watts pf power from PowerService')
+    this.powerService.supplyPower(20)
+    return 'data'
+  }
+}
