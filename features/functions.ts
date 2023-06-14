dfa2c33197bf82334bc1df59dabf8ebb52d18312
Stepan Poperechnyi <poperechnyi1@gmail.com>
2023-06-14 13:21:14 +0300

    add new projects

---
+++ features/functions.ts
@@ -0,0 +1,14 @@
+const add = (a: number, b: number): number => {
+  return a + b
+}
+
+interface forecast {
+  date: Date;
+  weather: string;
+}
+
+const logWeather = ({ date, weather }: forecast): void => {
+
+}
+
+
